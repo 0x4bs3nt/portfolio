3fb2f113bb20bced19664543c0f2edaf1702a609
0x4bs3nt <hi@4bs3nt.com>
2026-01-02 02:00:04 +0100

    theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [react(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: "github-dark",
+      theme: "github-dark-high-contrast",
       wrap: true,
     },
   },
